6027e6738f5bc7eb43cf7b77f2264ea09fc0d055
Mauro Belgiovine <mbelgiovine89@gmail.com>
2022-12-30 05:56:15 -0500

    Changed labels and improved accuracy plot

--- python/confusion_matrix.py
+++ python/confusion_matrix.py
@@ -17,7 +17,7 @@ logdir = args.logdir
 dirs = glob(logdir)
 
 exp_accuracies = []
-
+conf_matrices = []
 for d in sorted(dirs):
     figname = os.path.join(d, args.figname)
 
@@ -47,10 +47,12 @@ for d in sorted(dirs):
         else:
             conf_mat += cm
 
+    conf_mat = conf_mat / len(out)
+
     if True: #len(dirs) == 1:
         plt.clf()
-        axis_lbl = ['embb', 'mmtc', 'urll'] if conf_mat.shape[0] == 3 else ['embb', 'mmtc', 'urll', 'ctrl']
-        df_cm = pd.DataFrame(conf_mat/len(out), axis_lbl, axis_lbl)
+        axis_lbl = ['eMBB', 'mMTC', 'URLLC'] if conf_mat.shape[0] == 3 else ['eMBB', 'mMTC', 'URLLC', 'ctrl']
+        df_cm = pd.DataFrame(conf_mat, axis_lbl, axis_lbl)
         # plt.figure(figsize=(10,7))
         sn.set(font_scale=1.4) # for label size
         sn.heatmap(df_cm, annot=True, annot_kws={"size": 16}) # font size
@@ -61,6 +63,9 @@ for d in sorted(dirs):
     # compute average accuracy from all workers
     workers_accuracy = [c/t for c,t in zip(correct_samples, tot_samples)]
     exp_accuracies.append(np.mean(workers_accuracy)*100)
+    conf_matrices.append(conf_mat)
+
+
 
 if len(dirs) > 1:
     plt.clf()
@@ -68,11 +73,15 @@ if len(dirs) > 1:
     # plt.plot(exp_accuracies, 's--', label='4 class')
     # plt.xticks(np.arange(len(exp_accuracies)), ['4', '8', '16', '32'])
     sn.set(font_scale=1.2)  # for label size
-    plt.plot([4,8,16,32], exp_accuracies, 's--', label='4 class')
-    plt.xticks([4,8,16,32], ['4', '8', '16', '32'])
+    plt.plot([4,8,16,32,64], exp_accuracies, 's-', label='Tot. avg accuracy')
+    axis_lbl = zip(['eMBB', 'mMTC', 'URLLC'], ['^','o','>']) if conf_matrices[0].shape[0] == 3 else zip(['eMBB', 'mMTC', 'URLLC', 'ctrl'], ['^','o','>', 'p'])
+    for cix, lm in enumerate(axis_lbl):
+        class_acc = [conf_mat[cix,cix]*100 for conf_mat in conf_matrices]
+        plt.plot([4,8,16,32,64], class_acc, lm[1]+'--', label=lm[0])
+    plt.xticks([4,8,16,32,64], ['4', '8', '16', '32', '64'])
     plt.xlabel('Slice size')
     plt.ylabel('Accuracy (%)')
-    #plt.legend()
+    plt.legend()
     plt.grid(color='gray', linestyle='--')
     plt.savefig('4class_accuracy.png')
 
