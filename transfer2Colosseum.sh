b764cb54daf5e29e05ead37ac978885d6571a8ac
Mauro Belgiovine <mbelgiovine89@gmail.com>
2023-02-28 20:00:50 -0500

    Minor fix to change permission inside docker

--- transfer2Colosseum.sh
+++ transfer2Colosseum.sh
@@ -9,4 +9,4 @@ sshpass -p "ChangeMe" ssh $1 'docker cp /root/traffic_gen sample-xapp-24:/home/s
 sshpass -p "ChangeMe" ssh $1 'docker cp /root/logs/cols_maxmin.pkl sample-xapp-24:/home/sample-xapp/traffic_gen/logs/ && docker cp /root/logs/dataset__emuc__Trial1_Trial2_Trial3_Trial4_Trial5_Trial6__slice8_wCQI.pkl sample-xapp-24:/home/sample-xapp/traffic_gen/logs/'
 sshpass -p "ChangeMe" ssh $1 'docker cp /root/model/ sample-xapp-24:/home/sample-xapp/traffic_gen/.'
 sshpass -p "ChangeMe" ssh $1 'docker exec sample-xapp-24 ln -s /home/sample-xapp/xapp_control.py  /home/sample-xapp/traffic_gen/xapp_control.py'
-sshpass -p "ChangeMe" ssh $1 'docker exec sample-xapp-24 mv /home/sample-xapp/traffic_gen/run_xapp.sh /home/sample-xapp/. && chmod +x /home/sample-xapp/run_xapp.sh'
+sshpass -p "ChangeMe" ssh $1 'docker exec sample-xapp-24 mv /home/sample-xapp/traffic_gen/run_xapp.sh /home/sample-xapp/. && docker exec sample-xapp-24 chmod +x /home/sample-xapp/run_xapp.sh'
